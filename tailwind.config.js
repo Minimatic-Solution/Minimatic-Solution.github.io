48b635b8e9b40cae8785f1903e53a174493e5c04
Shahin Alom <shahinalomshuvo94@gmail.com>
2024-02-27 16:21:09 +0600

    update about company

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        5000: "5000ms",
+      },
+    },
   },
   plugins: [],
 };
