9c47e3a5ba4c4bb4e16594daab43f226003afd96
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2024-05-04 16:04:40 +0600

    update navigate

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ window.addEventListener('scroll', function() {
         navbar.style.opacity = '0.7';
     }
 });
+
+// smooth-scroll
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navbarHeight = document.querySelector('nav').offsetHeight;
+  
+    var scroll = new SmoothScroll('a[href*="#"]', {
+      speed: 800,
+      offset: navbarHeight 
+    });
+  });
+  
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            // Remove 'text-btnColor' class from all navigation items
+            navLinks.forEach(function (navLink) {
+                navLink.classList.remove('text-btnColor');
+            });
+
+            // Add 'text-btnColor' class to the clicked navigation item
+            link.classList.add('text-btnColor');
+        });
+    });
+});
