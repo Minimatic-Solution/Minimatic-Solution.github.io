928af725f2734b235db9269f30b4a7d25ec0306a
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2024-05-04 16:36:08 +0600

    add toggle active navbar

--- script.js
+++ script.js
@@ -12,27 +12,31 @@ window.addEventListener('scroll', function() {
 // smooth-scroll
 
 document.addEventListener('DOMContentLoaded', function () {
-    var navbarHeight = document.querySelector('nav').offsetHeight;
+    const navbarHeight = document.querySelector('nav').offsetHeight;
+    const navLinks = document.querySelectorAll('.nav-link');
+    const homeLink = document.querySelector('.nav-link[href="index.html"]');
+    homeLink.classList.add('text-btnColor');
   
-    var scroll = new SmoothScroll('a[href*="#"]', {
+    const scroll = new SmoothScroll('a[href*="#"]', {
       speed: 800,
       offset: navbarHeight 
+    });
+
+
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            if (link !== homeLink) {
+                navLinks.forEach(function (navLink) {
+                    navLink.classList.remove('text-btnColor');
+                });
+            }
+
+            link.classList.add('text-btnColor');
+        });
     });
   });
   
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var navLinks = document.querySelectorAll('.nav-link');
 
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            // Remove 'text-btnColor' class from all navigation items
-            navLinks.forEach(function (navLink) {
-                navLink.classList.remove('text-btnColor');
-            });
 
-            // Add 'text-btnColor' class to the clicked navigation item
-            link.classList.add('text-btnColor');
-        });
-    });
-});
